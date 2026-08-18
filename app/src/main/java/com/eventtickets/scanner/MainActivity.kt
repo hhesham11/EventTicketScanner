53fdcdd7913d078f4136d9cb27e35491c82bdc43
hhesham11 <hhesham381@gmail.com>
2026-08-18 05:23:17 +0300

    Update MainActivity.kt

--- app/src/main/java/com/eventtickets/scanner/MainActivity.kt
+++ app/src/main/java/com/eventtickets/scanner/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : ComponentActivity() {
         } else startCamera()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
